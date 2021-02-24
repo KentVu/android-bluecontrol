0d93ff9f322bef21c1d4931983ab218d1855477c
Vu Tran Kien <vutrankien.vn@gmail.com>
2021-02-24 22:12:48 +0900

    queryPairedDevices

--- app/src/main/java/com/vutrankien/bluecontrol/MainActivity.kt
+++ app/src/main/java/com/vutrankien/bluecontrol/MainActivity.kt
@@ -2,11 +2,16 @@ package com.vutrankien.bluecontrol
 
 import android.Manifest
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.View
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.DialogCompat
+import androidx.core.content.ContextCompat
 import com.vutrankien.android.lib.AndroidLogFactory
 import com.vutrankien.lib.LogFactory
 import kotlinx.coroutines.CoroutineScope
@@ -22,6 +27,8 @@ class MainActivity : AppCompatActivity() {
 
     val scope = CoroutineScope(Dispatchers.Main + Job())
 
+    private lateinit var bluetoothAdapter: BluetoothAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -35,12 +42,17 @@ class MainActivity : AppCompatActivity() {
     override fun onResume() {
         super.onResume()
         log.d("onResume")
-        val bluetoothAdapter: BluetoothAdapter? = BluetoothAdapter.getDefaultAdapter()
+        val bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
         if (bluetoothAdapter == null) {
             // Device doesn't support Bluetooth
+            AlertDialog.Builder(this)
+                .setMessage("Device does not support Bluetooth")
+                .setOnCancelListener { finish() }
+                .show()
             log.e("Device does not support Bluetooth")
             return
         }
+        this.bluetoothAdapter = bluetoothAdapter
         if (!bluetoothAdapter.isEnabled) {
             val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
             enableBluetoothLauncher.launch(enableBtIntent)
@@ -52,7 +64,29 @@ class MainActivity : AppCompatActivity() {
             log.i("User has granted permission: $isGranted")
         }
 
+    private val locationPermissionGranted: Boolean
+        get() = ContextCompat.checkSelfPermission(
+            this,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        ) == PackageManager.PERMISSION_GRANTED
+
     fun onDiscoverClick(view: View) {
-        permissionRequestLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        if (locationPermissionGranted) {
+            queryPairedDevices()
+        } else {
+            permissionRequestLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
+        }
+    }
+
+    private fun queryPairedDevices() {
+        val pairedDevices: Set<BluetoothDevice>? = bluetoothAdapter.bondedDevices
+        pairedDevices?.let {
+            log.d("Paired devices:${it.count()}")
+            it.forEach { device ->
+                val deviceName = device.name
+                val deviceHardwareAddress = device.address // MAC address
+                log.i("d:$device")
+            }
+        }
     }
 }
