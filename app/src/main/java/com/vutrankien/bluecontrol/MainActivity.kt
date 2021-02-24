7449684563b264a1cf2c5196e807612d5cb4d239
Vu Tran Kien <vutrankien.vn@gmail.com>
2021-02-24 21:32:37 +0900

    Use androidx.activity:activity-ktx registerForActivityResult

--- app/src/main/java/com/vutrankien/bluecontrol/MainActivity.kt
+++ app/src/main/java/com/vutrankien/bluecontrol/MainActivity.kt
@@ -1,18 +1,17 @@
 package com.vutrankien.bluecontrol
 
-import android.app.Activity
+import android.Manifest
 import android.bluetooth.BluetoothAdapter
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 import com.vutrankien.android.lib.AndroidLogFactory
 import com.vutrankien.lib.LogFactory
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.channels.Channel
-import kotlinx.coroutines.channels.first
-import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
 
@@ -20,15 +19,18 @@ class MainActivity : AppCompatActivity() {
         private val log: LogFactory.Log = AndroidLogFactory.instance.newLog("MainActivity")
         private const val REQUEST_ENABLE_BT = 1
     }
-    data class ActivityResult(val requestCode: Int, val resultCode: Int)
 
     val scope = CoroutineScope(Dispatchers.Main + Job())
-    private val activityResultChannel: Channel<ActivityResult> = Channel()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    private val enableBluetoothLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) {
+            log.i("Bluetooth enabled: $it")
+        }
 
     override fun onResume() {
         super.onResume()
@@ -39,21 +41,18 @@ class MainActivity : AppCompatActivity() {
             log.e("Device does not support Bluetooth")
             return
         }
-        scope.launch {
-            if (!bluetoothAdapter.isEnabled) {
-                val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
-                startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT)
-                if (activityResultChannel.first { it.requestCode == REQUEST_ENABLE_BT }.resultCode == Activity.RESULT_OK) {
-                    log.i("Bluetooth enabled")
-                }
-            }
+        if (!bluetoothAdapter.isEnabled) {
+            val enableBtIntent = Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE)
+            enableBluetoothLauncher.launch(enableBtIntent)
         }
     }
 
-    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
-        super.onActivityResult(requestCode, resultCode, data)
-        scope.launch {
-            activityResultChannel.send(ActivityResult(requestCode, resultCode))
+    private val permissionRequestLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted ->
+            log.i("User has granted permission: $isGranted")
         }
+
+    fun onDiscoverClick(view: View) {
+        permissionRequestLauncher.launch(Manifest.permission.ACCESS_FINE_LOCATION)
     }
 }
